e72442741df8ff8f80f8d33f6bbbdeb91324a762
Aaron Bittel <aaron@bittels.de>
2026-03-26 14:14:14 +0100

    feat: add multiple stdin && help and version

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <fcntl.h>
+#include <stddef.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -22,10 +23,10 @@ typedef struct {
 #define UNICODE_CONTINUATION_UPPER 0xBF       // 10 111111
 #define UNICODE_CONTINUATION_LOWER 0x80       // 10 000000
 
-#define C_FLAG 0x01 << 0
-#define L_FLAG 0x01 << 1
-#define W_FLAG 0x01 << 2
-#define M_FLAG 0x01 << 3
+#define FLAG_BYTES 0x01 << 0
+#define FLAG_LINES 0x01 << 1
+#define FLAG_WORDS 0x01 << 2
+#define FLAG_CHARS 0x01 << 3
 
 #define FILENAMES_CAPACITY 256
 
@@ -40,38 +41,37 @@ bool is_flag_provided(int flags, int flag);
 void process_word_count(char c, bool* in_word, size_t* word_count);
 void process_char_count(unsigned char c, size_t* exp_uc_cont_bytes, size_t* char_count);
 void print_wc_counts(int flags, size_t filesize, size_t line_count, size_t word_count, size_t char_count, const char* filename);
+void print_help();
+void print_version();
 
 int main(int argc, char** argv) {
     int flags = 0;
 
     for (size_t i = 1; i < (size_t)argc; i++) {
         if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--bytes") == 0) {
-            flags |= C_FLAG;
-        } else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--lines") == 0) { flags |= L_FLAG;
-        } else if (strcmp(argv[i], "-w") == 0 || strcmp(argv[1], "--words") == 0) {
-            flags |= W_FLAG;
-        } else if (strcmp(argv[i], "-m") == 0, || strcmp(argv[i], "--chars") == 0) {
-            flags |= M_FLAG;
+            flags |= FLAG_BYTES;
+        } else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--lines") == 0) { flags |= FLAG_LINES;
+        } else if (strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--words") == 0) {
+            flags |= FLAG_WORDS;
+        } else if (strcmp(argv[i], "-m") == 0 || strcmp(argv[i], "--chars") == 0) {
+            flags |= FLAG_CHARS;
+        } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            print_help();
+            return 0;
+        } else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
+            print_version();
+            return 0;
         } else {
             filenames[filenames_count++] = argv[i];
         }
     }
 
     if (flags == 0) { // no flags provided
-        flags = C_FLAG | L_FLAG | W_FLAG;
+        flags = FLAG_BYTES | FLAG_LINES | FLAG_WORDS;
     }
 
     // Read from stdin
-    if (filenames_count == 0) {
-        size_t filesize = 0;
-        size_t line_count = 0;
-        size_t word_count = 0;
-        size_t char_count = 0;
-        process_fd_counts(STDIN_FILENO, flags, &filesize, &line_count, &word_count, &char_count);
-        print_wc_counts(flags, filesize, line_count, word_count, char_count, NULL);
-        return 0;
-    }
-
+    if (filenames_count == 0) filenames[filenames_count++] = "-";
 
     size_t total_filesize = 0;
     size_t total_line_count = 0;
@@ -79,7 +79,7 @@ int main(int argc, char** argv) {
     size_t total_char_count = 0;
 
     for (size_t i = 0; i < filenames_count; i++) {
-        int fd = open(filenames[i], O_RDONLY);
+        int fd = strcmp(filenames[i], "-") == 0 ? STDIN_FILENO : open(filenames[i], O_RDONLY);
         if (fd < 0) perror("open failed");
 
         size_t filesize = 0;
@@ -95,7 +95,7 @@ int main(int argc, char** argv) {
         total_word_count += word_count;
         total_char_count += char_count;
 
-        close(fd);
+        if (fd != STDIN_FILENO) close(fd);
     }
 
     if (filenames_count > 1) {
@@ -103,6 +103,33 @@ int main(int argc, char** argv) {
     }
 
     return 0;
+}
+
+#define BUF_CAPACITY 8096
+
+void process_fd_counts(int fd, int flags, size_t* filesize, size_t* line_count, size_t* word_count, size_t* char_count) {
+    char buf[BUF_CAPACITY];
+
+    bool do_c = is_flag_provided(flags, FLAG_BYTES);
+    bool do_l = is_flag_provided(flags, FLAG_LINES);
+    bool do_w = is_flag_provided(flags, FLAG_WORDS);
+    bool do_m = is_flag_provided(flags, FLAG_CHARS);
+
+    size_t remaining_uc_bytes = 0;
+    bool in_word = false;
+
+    ssize_t n;
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        if (do_c) *filesize += n;
+        for (size_t i = 0; (ssize_t)i < n; i++) {
+            if (do_l && buf[i] == '\n') *line_count += 1;
+            if (do_w) process_word_count(buf[i], &in_word, word_count);
+            if (do_m) process_char_count((unsigned char)buf[i],
+                                         &remaining_uc_bytes, char_count);
+        }
+    }
+    if (in_word) *word_count += 1;
+    if (n == -1) perror("read failed");
 }
 
 size_t get_filesize(FILE* f) {
@@ -168,38 +195,27 @@ void process_char_count(unsigned char c, size_t* exp_uc_cont_bytes, size_t* char
     }
 }
 
-#define BUF_CAPACITY 8096
-
-void process_fd_counts(int fd, int flags, size_t* filesize, size_t* line_count, size_t* word_count, size_t* char_count) {
-    char buf[BUF_CAPACITY];
-
-    bool do_c = is_flag_provided(flags, C_FLAG);
-    bool do_l = is_flag_provided(flags, L_FLAG);
-    bool do_w = is_flag_provided(flags, W_FLAG);
-    bool do_m = is_flag_provided(flags, M_FLAG);
-
-    size_t remaining_uc_bytes = 0;
-    bool in_word = false;
-
-    ssize_t n;
-    while ((n = read(fd, buf, sizeof(buf))) > 0) {
-        if (do_c) *filesize += n;
-        for (size_t i = 0; (ssize_t)i < n; i++) {
-            if (do_l && buf[i] == '\n') *line_count += 1;
-            if (do_w) process_word_count(buf[i], &in_word, word_count);
-            if (do_m) process_char_count((unsigned char)buf[i],
-                                         &remaining_uc_bytes, char_count);
-        }
-    }
-    if (in_word) *word_count += 1;
-    if (n == -1) perror("read failed");
-}
-
 void print_wc_counts(int flags, size_t filesize, size_t line_count, size_t word_count, size_t char_count, const char* filename) {
-    if (is_flag_provided(flags, L_FLAG)) printf("%7zu", line_count);
-    if (is_flag_provided(flags, W_FLAG)) printf("%7zu", word_count);
-    if (is_flag_provided(flags, C_FLAG)) printf("%7zu", filesize);
-    if (is_flag_provided(flags, M_FLAG)) printf("%7zu", char_count);
+    if (is_flag_provided(flags, FLAG_LINES)) printf("%7zu", line_count);
+    if (is_flag_provided(flags, FLAG_WORDS)) printf("%7zu", word_count);
+    if (is_flag_provided(flags, FLAG_CHARS)) printf("%7zu", char_count);
+    if (is_flag_provided(flags, FLAG_BYTES)) printf("%7zu", filesize);
     if (filename != NULL) printf(" %s", filename);
     printf("\n");
+}
+
+void print_help() {
+    printf("Usage: ccwc [OPTION]... [FILE]...\n");
+    printf("Print newline, word, and byte counts for each FILE, or standard input if none are given.\n\n");
+    printf("Options:\n");
+    printf("  -c, --bytes     print the byte counts\n");
+    printf("  -l, --lines     print the newline counts\n");
+    printf("  -w, --words     print the word counts\n");
+    printf("  -m, --chars     print the character counts\n");
+    printf("  -h, --help      display this help and exit\n");
+    printf("  -v, --version   output version information and exit\n");
+}
+
+void print_version() {
+    printf("ccwc version 1.0.0\n");
 }
